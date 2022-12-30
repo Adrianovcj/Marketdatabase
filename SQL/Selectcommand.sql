7858e615506d7f1627ec12da9a01435c4ccbb4e0
Adrianovcj <adrianovcj@gmail.com>
2022-12-30 17:59:12 -0400

    Add files via upload

---
+++ SQL/Selectcommand.sql
@@ -0,0 +1,15 @@
+select * from cliente;
+
+select CPF, NOME FROM Cliente;
+
+select CPF, NOME FROM Cliente LIMIT 1;
+
+select CPF as CPF_CLIENTE, NOME as NOME_CLIENTE from  cliente;
+
+
+SELECT * from Cliente WHERE IDADE = '20';
+
+
+UPDATE Cliente SET IDADE = 18 WHERE IDADE = 20
+
+select * from cliente
---
+++ SQL/alter&insertcomands.sql
@@ -0,0 +1,65 @@
+USE sucos2;
+
+ALTER TABLE Cliente ADD PRIMARY KEY (CPF);
+
+ALTER TABLE Cliente ADD column (DATA_NASCIMENTO DATE);
+
+INSERT INTO cliente (
+CPF, 
+NOME, 
+ENDERECO1, 
+ENDERECO2, 
+BAIRRO, 
+CIDADE, 
+ESTADO, 
+CEP, 
+IDADE, 
+SEXO, 
+LIMITE_CREDITO, 
+VOLUME_COMPRA, 
+PRIMEIRA_COMPRA, 
+DATA_NASCIMENTO) VALUES (
+'00388934505', 
+'João da Silva', 
+'Rua projetada A número 10', 
+'', 
+'Vila Roman', 
+'CARATINGA', 
+'Amazonas', 
+'2222222', 
+30, 
+'M', 
+10000.00, 
+2000, 
+0, 
+'1989-10-05');
+
+INSERT INTO cliente (
+CPF, 
+NOME, 
+ENDERECO1, 
+ENDERECO2, 
+BAIRRO, 
+CIDADE, 
+ESTADO, 
+CEP, 
+IDADE, 
+SEXO, 
+LIMITE_CREDITO, 
+VOLUME_COMPRA, 
+PRIMEIRA_COMPRA, 
+DATA_NASCIMENTO) VALUES (
+'00388934500', 
+'Joana da Silva', 
+'Rua projetada A número 10', 
+'', 
+'Vila Roman', 
+'CARATINGA', 
+'Amazonas', 
+'2452222', 
+20, 
+'F', 
+10000.00, 
+2000, 
+0, 
+'1989-11-08');
